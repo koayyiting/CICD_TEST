4ee9ad32ec925b37ca286c148ab6655c25e3c63e
koayyiting <s10221765@connect.np.edu.sg>
2024-02-03 03:52:16 +0800

    test for console

--- console/console.go
+++ console/console.go
@@ -11,4 +11,3 @@ func main() {
 	account.InitHTTPServer()
 	record.InitHTTPServer()
 }
-
---
+++ console/console_test.go
@@ -0,0 +1,11 @@
+// console_test.go
+
+package main
+
+import (
+	"testing"
+)
+
+func TestMainFunction(t *testing.T) {
+	main() // Pass mocks to main
+}
